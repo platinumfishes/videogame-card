77808b2ab31e2404c6fb77938baa9a3e2ee2281f
Platinumfish <mulerotmglel@gmail.com>
2023-03-21 02:02:45 -0400

    commit

--- api/roster.js
+++ api/roster.js
@@ -1,10 +1,11 @@
+
 export default async function handler(request, res) {
+    
     const roster = [
         {
             "accentColor": "teamfortress",
             "picture": "https://i.ytimg.com/vi/jHgZh4GV9G0/maxresdefault.jpg",
             "name": "Heavy",
-            "opened": false,
             "toptext": "I am Heavy Weapons guy",
             "detailstext": "Details",
             "paragraph1": "Some people think they can outsmart me. Maybe, maybe... I've yet to meet one that can outsmart bullet."
@@ -13,7 +14,6 @@ export default async function handler(request, res) {
             "accentColor": "pink",
             "picture": "https://i.ytimg.com/vi/z8crH4zBPbk/maxresdefault.jpg",
             "name": "Soldier",
-            "opened": false,
             "toptext": "God bless America",
             "detailstext": "Details",
             "paragraph1": "This is a screenshot from a 3D rendered animation posted to Youtube by Valve Corporation to promote the video-game Team Fortress 2. The video is called 'Meet the Pyro'"
@@ -22,11 +22,15 @@ export default async function handler(request, res) {
             "accentColor": "pink",
             "picture": "https://i.ytimg.com/vi/bgs9OhjAE2g/maxresdefault.jpg",
             "name": "Spy",
-            "opened": false,
             "toptext": "Kill me",
             "detailstext": "Details",
             "paragraph1": "This is a screenshot from a 3D rendered animation posted to Youtube by Valve Corporation to promote the video-game Team Fortress 2. The video is called 'Meet the Pyro'"
         }
     ];
+    res.setHeader('Cache-Control', 'max-age=0, s-maxage=1800');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
     res.json(roster);
 }
